5b990a3a1b32937cad455ab6f92743615c1a1fff
blameaimer <blameaimer@gmail.com>
2022-03-11 14:44:21 +0000

    Error handling

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
           <Route path="/" element={<HomeArticle />} />
           <Route path="/articles/:topic" element={<ArticlesList />} />
           <Route path="/articles/:topic/:id" element={<Articel />} />
-          <Route path="/:id" element={<Articel />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/components/Articel-card.jsx
+++ src/components/Articel-card.jsx
@@ -1,84 +1,99 @@
-  import {Card,Button} from "react-bootstrap";
-  import {FaRegCommentDots} from 'react-icons/fa'
-  import {MdOutlineCreate,MdUpdate} from 'react-icons/md'
-  import {BiUpvote,BiDownvote} from 'react-icons/bi'
-  import { Link } from "react-router-dom";
-  import { useState } from "react";
-  import {updateVote} from '../api'
-export const ArticelCard =({article})=>{
-  const [vote,setVote]=useState(article.votes)
-  const [upvote,setUpvote]=useState(null)
-  const [downvote,setDownvote]=useState(null)
+import { Card, Button } from "react-bootstrap";
+import { FaRegCommentDots } from "react-icons/fa";
+import { MdOutlineCreate, MdUpdate } from "react-icons/md";
+import { BiUpvote, BiDownvote } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { updateVote } from "../api";
+export const ArticelCard = ({ article }) => {
+  const [vote, setVote] = useState(article.votes);
+  const [upvote, setUpvote] = useState(null);
+  const [downvote, setDownvote] = useState(null);
   const [err, setErr] = useState(null);
 
   const handleUpVote = (id) => {
-if(downvote)  {
-  setVote((currVote)=>currVote+2)
-  updateVote(id,2).catch((err) => {
-    // setVote((currVote) => currVote -= 1);
-    setErr('Something went wrong, please try again.');
-  });
-} 
-  else{ 
-    setVote((currVote)=>currVote+1)
-    updateVote(id,1).catch((err) => {
-      // setVote((currVote) => currVote -= 1);
-      setErr('Something went wrong, please try again.');
-    });
-  }
-setUpvote(true);
-setDownvote(null)
-
-
- };
+    if (downvote) {
+      setVote((currVote) => currVote + 2);
+      updateVote(id, 2).catch((err) => {
+        // setVote((currVote) => currVote -= 1);
+        setErr("Something went wrong, please try again.");
+      });
+    } else {
+      setVote((currVote) => currVote + 1);
+      updateVote(id, 1).catch((err) => {
+        // setVote((currVote) => currVote -= 1);
+        setErr("Something went wrong, please try again.");
+      });
+    }
+    setUpvote(true);
+    setDownvote(null);
+  };
   const handleDownVote = (id) => {
- if(upvote){
-   setVote((currVote)=>currVote-2)
-   updateVote(id,-2).catch((err) => {
-    // setVote((currVote) => currVote -= 1);
-    setErr('Something went wrong, please try again.');
-  });
-
-
-} else {
-  setVote((currVote)=>currVote-1)
-  updateVote(id,-1).catch((err) => {
-    // setVote((currVote) => currVote -= 1);
-    setErr('Something went wrong, please try again.');
-  });
-}
-setDownvote(true)
-setUpvote(null)
-    
+    if (upvote) {
+      setVote((currVote) => currVote - 2);
+      updateVote(id, -2).catch((err) => {
+        // setVote((currVote) => currVote -= 1);
+        setErr("Something went wrong, please try again.");
+      });
+    } else {
+      setVote((currVote) => currVote - 1);
+      updateVote(id, -1).catch((err) => {
+        // setVote((currVote) => currVote -= 1);
+        setErr("Something went wrong, please try again.");
+      });
+    }
+    setDownvote(true);
+    setUpvote(null);
   };
 
-return(
-<Card
-    bg='dark'
-    text='light'
-    style={{ width: '100%'}}
-    
-    className={`${article.article_id}`}
-  >
-    <Card.Header ><Link style={{ textDecoration: 'none' }} to={{
-
-    pathname: `${article.article_id}`
-  }}><p className="title">{article.title} </p></Link>
-    <p className="date"><MdUpdate/>{article.created_at.slice(0,-14)}</p></Card.Header>
-
-    <Card.Body>
-      <Card.Text>
-        {article.body}
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer
+  return (
+    <Card
+      bg="dark"
+      text="light"
+      style={{ width: "100%" }}
+      className={`${article.article_id}`}
     >
-     
-       <p className="author-comments"><MdOutlineCreate/>{article.author} <FaRegCommentDots className="comment-icon"/>  {vote} <Button disabled={upvote} onClick={()=>handleUpVote(article.article_id)}variant="dark"> <BiUpvote className="upvote"/></Button> <Button disabled={downvote} onClick={()=>handleDownVote(article.article_id)} variant="dark"><BiDownvote className="downvote"/></Button></p>  
+      <Card.Header>
+        <Link
+          style={{ textDecoration: "none" }}
+          to={{
+            pathname: `/articles/${article.topic}/${article.article_id}`,
+          }}
+        >
+          <p className="title">{article.title} </p>
+        </Link>
+        <p className="date">
+          <MdUpdate />
+          {article.created_at.slice(0, -14)}
+        </p>
+      </Card.Header>
 
-    </Card.Footer>
-  </Card>
-)
-}
+      <Card.Body>
+        <Card.Text>{article.body}</Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <p className="author-comments">
+          <MdOutlineCreate />
+          {article.author} <FaRegCommentDots className="comment-icon" /> {vote}{" "}
+          <Button
+            disabled={upvote}
+            onClick={() => handleUpVote(article.article_id)}
+            variant="dark"
+          >
+            {" "}
+            <BiUpvote className="upvote" />
+          </Button>{" "}
+          <Button
+            disabled={downvote}
+            onClick={() => handleDownVote(article.article_id)}
+            variant="dark"
+          >
+            <BiDownvote className="downvote" />
+          </Button>
+        </p>
+      </Card.Footer>
+    </Card>
+  );
+};
 
-export default ArticelCard
+export default ArticelCard;
